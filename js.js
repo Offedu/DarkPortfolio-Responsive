c59b6ec849fff5dc1d0c41bffd208620957937f0
edu <131923154+Offedu@users.noreply.github.com>
2023-08-16 00:07:03 -0300

    Add files via upload
    working...

---
+++ js.js
@@ -0,0 +1,7 @@
+new TypeIt("#simpleUsage", {
+    strings: ["I am developer.", "I am studying.", "<span class='htmljs'>HTML<span class='virgulajs'>, <span class='cssjs'>CSS<span class='virgulajs'> and <span class='jsjs'>JS<span class='virgulajs'>.</span>"],
+    speed: 50,
+    waitUntilVisible: true,
+    loop: true,
+    loopDelay: 1000,
+}).go();
